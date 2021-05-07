2c5ad9f408ace7caef5005c1ac19cd93ad66912b
Anusri Chaurasia <anusricc8@gmail.com>
2021-05-07 09:19:11 +0530

    sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,53 @@
-import 'package:capstone/Screens/Welcome/welcome_screen.dart';
-import 'package:flutter/material.dart';
-//import 'package:capstone/nav.dart';
+//import 'package:capstone/Screens/Welcome/welcome_screen.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+//import 'package:capstone/nav.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key : key);
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  firebase_auth.FirebaseAuth firebaseAuth = firebase_auth.FirebaseAuth.instance;
+
+  void signup() async{
+    try{
+      await firebaseAuth.createUserWithEmailAndPassword(email: "anusrichaurasia@gmail.com", password: "anusri234");
+    }
+    catch(e){
+      print(e);
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("firebase"),
+        ),
+        body: Center(
+          child: RaisedButton(
+            onPressed: () {
+              signup();
+            },
+            child: Text("Signup"),
+          )
+        ),
+      ),
+    );
+  }
+}
+
+/*class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -20,5 +61,5 @@ class MyApp extends StatelessWidget {
       home: WelcomeScreen(),
     );
   }
-}
+}*/
 

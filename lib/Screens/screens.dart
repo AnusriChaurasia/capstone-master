b7d0e7a8d87ef6d84f71bf82e76724b6136aad22
17BTCS023 <manisha.17cs@cmr.edu.in>
2021-05-09 14:31:09 +0530

    export screens added

---
+++ lib/Screens/screens.dart
@@ -0,0 +1,6 @@
+export 'Login/login_screen.dart';
+export 'Profile/profile_screen.dart';
+export 'Learn/learn_screen.dart';
+export 'Invest/invest_screen.dart';
+export 'Signup/signup_screen.dart';
+export 'Welcome/welcome_screen.dart';
